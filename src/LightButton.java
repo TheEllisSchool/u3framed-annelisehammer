bde1d1118a46f542f73d9d87a9cd4627c623c73c
annelisehammer <annelisehammer@172.16.21.61>
2020-02-03 20:29:24 -0500

    Finished :)

--- src/LightButton.java
+++ src/LightButton.java
@@ -7,23 +7,35 @@ public class LightButton extends JButton {
     private static final int MAXSIZE = 100;
     private int row = 0;
     private int col = 0;
-    
+    private boolean isLit;
+   
+   
     public LightButton (int r, int c){
         row = r;
         col = c;
         setBackground(Color.BLACK);
-        
+       
         Dimension size = new Dimension(MAXSIZE, MAXSIZE);
         setPreferredSize(size);
     }
-    
+   
+    public void reset() {
+isLit = false;
+setBackground(Color.BLACK);
+setText("");
+
+}
+
+   
     public int getRow(){
         return row;
     }
     public int getCol(){
         return col;
     }
-    
+   
+   
+   
     public void turnOn(){
         setBackground(Color.RED);
         setOpaque(true);
@@ -32,14 +44,14 @@ public class LightButton extends JButton {
         setBackground(Color.BLACK);
         setOpaque(true);
     }
-    
+   
     public boolean isLit(){
         Color color = getBackground();
         boolean isLit = (color == Color.RED);
         return isLit;        
     }
-    
-    
+   
+   
     public void toggle(){
         if (isLit()){
             turnOff();
